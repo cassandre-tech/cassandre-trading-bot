2c75751d996c19c308d1b0829adb8242f0e00b1c
Stéphane Traumat <stephane.traumat@gmail.com>
2020-02-27 17:09:16 +0100

    Maven archetype - closes #34

---
+++ trading-bot-spring-boot-starter-archetype/src/main/resources/archetype-resources/src/main/java/package-info.java
@@ -0,0 +1,7 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Simple strategy.
+ */
+package ${package};
---
+++ trading-bot-spring-boot-starter-archetype/src/main/resources/archetype-resources/src/test/java/SimpleStrategyTest.java
@@ -0,0 +1,45 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tech.cassandre.trading.bot.dto.market.TickerDTO;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.with;
+import static org.awaitility.pollinterval.FibonacciPollInterval.fibonacci;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Simple strategy test.
+ */
+@SpringBootTest
+@DisplayName("Simple strategy test")
+public class SimpleStrategyTest {
+
+	/** How much we should wait for tests to last. */
+	protected static final long MAXIMUM_RESPONSE_TIME_IN_SECONDS = 60;
+
+	/** Dumb strategy. */
+	@Autowired
+	SimpleStrategy strategy;
+
+	/**
+	 * Check that the strategy receive data.
+	 */
+	@Test
+	@DisplayName("Check data reception")
+	public void receivedData() {
+		// Waiting to see if the strategy received the accounts update (we have two accounts).
+		with().pollInterval(fibonacci(SECONDS)).await()
+				.atMost(MAXIMUM_RESPONSE_TIME_IN_SECONDS, SECONDS)
+				.untilAsserted(() -> assertEquals(strategy.getAccounts().size(), 2));
+	}
+
+}
