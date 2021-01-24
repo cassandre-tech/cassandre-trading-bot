5a4b40105c32311d4ec26abc914fee2e820f4e23
straumat <stephane.traumat@gmail.com>
2021-01-24 21:34:25 +0100

    Review dto tests - closes #389

--- spring-boot-starter/autoconfigure/src/test/java/tech/cassandre/trading/bot/test/dto/OrderDTOTest.java
+++ spring-boot-starter/autoconfigure/src/test/java/tech/cassandre/trading/bot/test/dto/OrderDTOTest.java
@@ -4,7 +4,6 @@ import io.qase.api.annotation.CaseId;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import tech.cassandre.trading.bot.dto.trade.OrderDTO;
-import tech.cassandre.trading.bot.dto.trade.OrderStatusDTO;
 import tech.cassandre.trading.bot.dto.util.CurrencyAmountDTO;
 import tech.cassandre.trading.bot.dto.util.CurrencyPairDTO;
 
@@ -65,10 +64,10 @@ public class OrderDTOTest {
 		assertEquals(order01, order02);
 		assertEquals(order02, order01);
 
-		// Order 3 - type changed.
+		// Order 3 - order id changed.
 		OrderDTO order03 = OrderDTO.builder()
-				.orderId("000001")
-				.type(BID)
+				.orderId("000002")
+				.type(ASK)
 				.currencyPair(cp1)
 				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
 				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
@@ -82,12 +81,12 @@ public class OrderDTOTest {
 		assertNotEquals(order01, order03);
 		assertNotEquals(order03, order01);
 
-		// Order 4 - original amount changed.
+		// Order 4 - type changed.
 		OrderDTO order04 = OrderDTO.builder()
 				.orderId("000001")
 				.type(BID)
 				.currencyPair(cp1)
-				.amount(new CurrencyAmountDTO("9", cp1.getBaseCurrency()))
+				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
 				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
 				.limitPrice(new CurrencyAmountDTO("5", cp1.getQuoteCurrency()))
 				.leverage("leverage1")
@@ -102,7 +101,7 @@ public class OrderDTOTest {
 		// Order 5 - currency pair changed.
 		OrderDTO order05 = OrderDTO.builder()
 				.orderId("000001")
-				.type(BID)
+				.type(ASK)
 				.currencyPair(cp2)
 				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
 				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
@@ -113,13 +112,16 @@ public class OrderDTOTest {
 				.userReference("MY_REF_1")
 				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
 				.build();
+		assertNotEquals(order01, order05);
+		assertNotEquals(order03, order05);
+		assertNotEquals(order05, order03);
 
-		// Order 6 - id changed.
+		// Order 6 - amount changed.
 		OrderDTO order06 = OrderDTO.builder()
-				.orderId("000002")
-				.type(ASK)
+				.orderId("000001")
+				.type(BID)
 				.currencyPair(cp1)
-				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
+				.amount(new CurrencyAmountDTO("9", cp1.getBaseCurrency()))
 				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
 				.limitPrice(new CurrencyAmountDTO("5", cp1.getQuoteCurrency()))
 				.leverage("leverage1")
@@ -130,79 +132,9 @@ public class OrderDTOTest {
 				.build();
 		assertNotEquals(order01, order06);
 		assertNotEquals(order06, order01);
-		assertNotEquals(order01, order05);
-		assertNotEquals(order05, order01);
 
-		// Order 7 - user reference changed.
+		// Order 7 - average price changed.
 		OrderDTO order07 = OrderDTO.builder()
-				.orderId("000001")
-				.type(ASK)
-				.currencyPair(cp1)
-				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
-				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
-				.limitPrice(new CurrencyAmountDTO("5", cp1.getQuoteCurrency()))
-				.leverage("leverage1")
-				.status(NEW)
-				.cumulativeAmount(new CurrencyAmountDTO("2", cp1.getBaseCurrency()))
-				.userReference("MY_REF_2")
-				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
-				.build();
-		assertNotEquals(order01, order07);
-		assertNotEquals(order07, order01);
-
-		// Order 8 - timestamp changed.
-		OrderDTO order08 = OrderDTO.builder()
-				.orderId("000001")
-				.type(ASK)
-				.currencyPair(cp1)
-				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
-				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
-				.limitPrice(new CurrencyAmountDTO("5", cp1.getQuoteCurrency()))
-				.leverage("leverage1")
-				.status(NEW)
-				.cumulativeAmount(new CurrencyAmountDTO("2", cp1.getBaseCurrency()))
-				.userReference("MY_REF_1")
-				.timestamp(ZonedDateTime.of(2018, 2, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
-				.build();
-		assertNotEquals(order01, order08);
-		assertNotEquals(order08, order01);
-
-		// Order 9 - status changed.
-		OrderDTO order09 = OrderDTO.builder()
-				.orderId("000001")
-				.type(ASK)
-				.currencyPair(cp1)
-				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
-				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
-				.limitPrice(new CurrencyAmountDTO("5", cp1.getQuoteCurrency()))
-				.leverage("leverage1")
-				.status(PENDING_NEW)
-				.cumulativeAmount(new CurrencyAmountDTO("2", cp1.getBaseCurrency()))
-				.userReference("MY_REF_1")
-				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
-				.build();
-		assertNotEquals(order01, order09);
-		assertNotEquals(order09, order01);
-
-		// Order 10 - cumulative amount changed.
-		OrderDTO order10 = OrderDTO.builder()
-				.orderId("000001")
-				.type(ASK)
-				.currencyPair(cp1)
-				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
-				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
-				.limitPrice(new CurrencyAmountDTO("5", cp1.getQuoteCurrency()))
-				.leverage("leverage1")
-				.status(NEW)
-				.cumulativeAmount(new CurrencyAmountDTO("9", cp1.getBaseCurrency()))
-				.userReference("MY_REF_1")
-				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
-				.build();
-		assertNotEquals(order01, order10);
-		assertNotEquals(order10, order01);
-
-		// Order 11 - average price changed.
-		OrderDTO order11 = OrderDTO.builder()
 				.orderId("000001")
 				.type(ASK)
 				.currencyPair(cp1)
@@ -215,11 +147,28 @@ public class OrderDTOTest {
 				.userReference("MY_REF_1")
 				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
 				.build();
-		assertNotEquals(order01, order11);
-		assertNotEquals(order11, order01);
+		assertNotEquals(order01, order07);
+		assertNotEquals(order07, order01);
 
-		// Order 12 - leverage changed.
-		OrderDTO order12 = OrderDTO.builder()
+		// Order 8 - limit price changed.
+		OrderDTO order08 = OrderDTO.builder()
+				.orderId("000001")
+				.type(ASK)
+				.currencyPair(cp1)
+				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
+				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
+				.limitPrice(new CurrencyAmountDTO("9", cp1.getQuoteCurrency()))
+				.leverage("leverage1")
+				.status(NEW)
+				.cumulativeAmount(new CurrencyAmountDTO("2", cp1.getBaseCurrency()))
+				.userReference("MY_REF_1")
+				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
+				.build();
+		assertNotEquals(order01, order08);
+		assertNotEquals(order08, order01);
+
+		// Order 9 - leverage changed.
+		OrderDTO order09 = OrderDTO.builder()
 				.orderId("000001")
 				.type(ASK)
 				.currencyPair(cp1)
@@ -230,6 +179,57 @@ public class OrderDTOTest {
 				.status(NEW)
 				.cumulativeAmount(new CurrencyAmountDTO("2", cp1.getBaseCurrency()))
 				.userReference("MY_REF_1")
+				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
+				.build();
+		assertNotEquals(order01, order09);
+		assertNotEquals(order09, order01);
+
+		// Order 10 - status changed.
+		OrderDTO order10 = OrderDTO.builder()
+				.orderId("000001")
+				.type(ASK)
+				.currencyPair(cp1)
+				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
+				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
+				.limitPrice(new CurrencyAmountDTO("5", cp1.getQuoteCurrency()))
+				.leverage("leverage1")
+				.status(PENDING_NEW)
+				.cumulativeAmount(new CurrencyAmountDTO("2", cp1.getBaseCurrency()))
+				.userReference("MY_REF_1")
+				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
+				.build();
+		assertNotEquals(order01, order10);
+		assertNotEquals(order10, order01);
+
+		// Order 11 - cumulative amount changed.
+		OrderDTO order11 = OrderDTO.builder()
+				.orderId("000001")
+				.type(ASK)
+				.currencyPair(cp1)
+				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
+				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
+				.limitPrice(new CurrencyAmountDTO("5", cp1.getQuoteCurrency()))
+				.leverage("leverage1")
+				.status(NEW)
+				.cumulativeAmount(new CurrencyAmountDTO("9", cp1.getBaseCurrency()))
+				.userReference("MY_REF_1")
+				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
+				.build();
+		assertNotEquals(order01, order11);
+		assertNotEquals(order11, order01);
+
+		// Order 12 - user reference changed.
+		OrderDTO order12 = OrderDTO.builder()
+				.orderId("000001")
+				.type(ASK)
+				.currencyPair(cp1)
+				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
+				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
+				.limitPrice(new CurrencyAmountDTO("5", cp1.getQuoteCurrency()))
+				.leverage("leverage1")
+				.status(NEW)
+				.cumulativeAmount(new CurrencyAmountDTO("2", cp1.getBaseCurrency()))
+				.userReference("MY_REF_2")
 				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
 				.build();
 		assertNotEquals(order01, order12);
@@ -243,31 +243,14 @@ public class OrderDTOTest {
 				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
 				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
 				.limitPrice(new CurrencyAmountDTO("5", cp1.getQuoteCurrency()))
-				.leverage("leverage2")
-				.status(NEW)
-				.cumulativeAmount(new CurrencyAmountDTO("2", cp1.getBaseCurrency()))
-				.userReference("MY_REF_1")
-				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
-				.build();
-		assertNotEquals(order01, order13);
-		assertNotEquals(order13, order01);
-
-		// Order 14 - limit price changed.
-		OrderDTO order14 = OrderDTO.builder()
-				.orderId("000001")
-				.type(ASK)
-				.currencyPair(cp1)
-				.amount(new CurrencyAmountDTO("1", cp1.getBaseCurrency()))
-				.averagePrice(new CurrencyAmountDTO("3", cp1.getQuoteCurrency()))
-				.limitPrice(new CurrencyAmountDTO("9", cp1.getQuoteCurrency()))
 				.leverage("leverage1")
 				.status(NEW)
 				.cumulativeAmount(new CurrencyAmountDTO("2", cp1.getBaseCurrency()))
 				.userReference("MY_REF_1")
-				.timestamp(ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC")))
+				.timestamp(ZonedDateTime.of(2018, 1, 3, 0, 0, 0, 0, ZoneId.of("UTC")))
 				.build();
-		assertNotEquals(order01, order14);
-		assertNotEquals(order14, order01);
+		assertNotEquals(order01, order13);
+		assertNotEquals(order13, order01);
 
 		// Tests for null objects.
 		OrderDTO order15 = OrderDTO.builder().build();
