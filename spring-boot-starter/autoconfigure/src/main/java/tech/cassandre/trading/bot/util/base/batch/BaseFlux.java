62426ade41b81355be699c17b4e4af5829e81361
straumat <stephane.traumat@gmail.com>
2021-06-16 20:21:44 +0200

    Refactor cassandre tests - Trying to fix CI - #600

--- spring-boot-starter/autoconfigure/src/main/java/tech/cassandre/trading/bot/util/base/batch/BaseFlux.java
+++ spring-boot-starter/autoconfigure/src/main/java/tech/cassandre/trading/bot/util/base/batch/BaseFlux.java
@@ -27,7 +27,7 @@ public abstract class BaseFlux<T> extends Base {
      */
     public BaseFlux() {
         Flux<Set<T>> fluxTemp = Flux.create(newFluxSink -> this.fluxSink = newFluxSink, getOverflowStrategy());
-        flux = fluxTemp.publishOn(Schedulers.boundedElastic());
+        flux = fluxTemp.publishOn(Schedulers.elastic());
     }
 
     /**
