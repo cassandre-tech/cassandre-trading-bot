0280260f6d735078be98d5a1ac78a45c8bb4d053
straumat <stephane.traumat@gmail.com>
2021-09-22 11:02:26 +0200

    Pre-release tasks - #748

--- util/test/api/graphql/wong_api_key.test.js
+++ util/test/api/graphql/wong_api_key.test.js
@@ -5,7 +5,7 @@ test("Accessing API with wrong API key", () => {
 	// The query of the GraphQL API server.
 	return fetch("http://localhost:8080/graphql", {
 		method: "POST",
-		headers: { "Content-Type": "application/json", "X-API-Key": "667341fd-d4c2-4bc2-99af-0a2a697aa134" },
+		headers: { "Content-Type": "application/json", "X-API-Key": "WRONG-API-KEY" },
 		body: JSON.stringify({ query: 
 			`query {
 				strategy(id:1){ strategyId name }
