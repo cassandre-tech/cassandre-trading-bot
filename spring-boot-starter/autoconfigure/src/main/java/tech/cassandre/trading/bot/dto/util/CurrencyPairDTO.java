8c016b333dc147e9fc509d0c5af1f8aec82e3e9b
xblaatx <tomsmeets@pandora.be>
2021-12-01 11:27:32 +0100

    checkstyles

--- spring-boot-starter/autoconfigure/src/main/java/tech/cassandre/trading/bot/dto/util/CurrencyPairDTO.java
+++ spring-boot-starter/autoconfigure/src/main/java/tech/cassandre/trading/bot/dto/util/CurrencyPairDTO.java
@@ -18,14 +18,35 @@ import java.util.Objects;
 @SuppressWarnings("checkstyle:VisibilityModifier")
 public class CurrencyPairDTO {
 
-    /** Currency pair separator. */
+    /**
+     * Currency pair separator.
+     */
     private static final String CURRENCY_PAIR_SEPARATOR = "/";
 
-    /** The base currency is the first currency appearing in a currency pair quotation. */
+    /**
+     * Currency pair default precision.
+     */
+    private static final Integer DEFAULT_CURRENCY_PRECISION = 8;
+
+    /**
+     * The base currency is the first currency appearing in a currency pair quotation.
+     */
     CurrencyDTO baseCurrency;
 
-    /** The quote currency is the second currency appearing in a currency pair quotation. */
+    /**
+     * The quote currency is the second currency appearing in a currency pair quotation.
+     */
     CurrencyDTO quoteCurrency;
+
+    /**
+     * The base currency precision.
+     */
+    int baseCurrencyPrecision;
+
+    /**
+     * The qoute currency precision.
+     */
+    int qouteCurrencyPrecision;
 
     /**
      * Constructor.
@@ -52,7 +73,19 @@ public class CurrencyPairDTO {
      * @param newQuoteCurrency The quote currency
      */
     public CurrencyPairDTO(final String newBaseCurrency, final String newQuoteCurrency) {
-        this(CurrencyDTO.getInstance(newBaseCurrency), CurrencyDTO.getInstance(newQuoteCurrency));
+        this(CurrencyDTO.getInstance(newBaseCurrency), CurrencyDTO.getInstance(newQuoteCurrency), DEFAULT_CURRENCY_PRECISION, DEFAULT_CURRENCY_PRECISION);
+    }
+
+    /**
+     * Constructor with {@link CurrencyDTO}.
+     *
+     * @param newBaseCurrency           The base currency
+     * @param newQuoteCurrency          The quote currency
+     * @param newBaseCurrencyPrecision  the base currency precision
+     * @param newQouteCurrencyPrecision the qoute currency precision
+     */
+    public CurrencyPairDTO(final String newBaseCurrency, final String newQuoteCurrency, final int newBaseCurrencyPrecision, final int newQouteCurrencyPrecision) {
+        this(CurrencyDTO.getInstance(newBaseCurrency), CurrencyDTO.getInstance(newQuoteCurrency), newBaseCurrencyPrecision, newQouteCurrencyPrecision);
     }
 
     /**
@@ -62,8 +95,22 @@ public class CurrencyPairDTO {
      * @param newQuoteCurrency The quote currency
      */
     public CurrencyPairDTO(final CurrencyDTO newBaseCurrency, final CurrencyDTO newQuoteCurrency) {
+        this(newBaseCurrency, newQuoteCurrency, DEFAULT_CURRENCY_PRECISION, DEFAULT_CURRENCY_PRECISION);
+    }
+
+    /**
+     * Constructor with String.
+     *
+     * @param newBaseCurrency           The base currency
+     * @param newQuoteCurrency          The quote currency
+     * @param newBaseCurrencyPrecision  the base currency precision
+     * @param newQouteCurrencyPrecision the qoute currency precision
+     */
+    public CurrencyPairDTO(final CurrencyDTO newBaseCurrency, final CurrencyDTO newQuoteCurrency, final int newBaseCurrencyPrecision, final int newQouteCurrencyPrecision) {
         this.baseCurrency = newBaseCurrency;
         this.quoteCurrency = newQuoteCurrency;
+        this.baseCurrencyPrecision = newBaseCurrencyPrecision;
+        this.qouteCurrencyPrecision = newQouteCurrencyPrecision;
     }
 
     /**
@@ -75,6 +122,23 @@ public class CurrencyPairDTO {
         final CurrencyPair cp = (CurrencyPair) instrument;
         this.baseCurrency = new CurrencyDTO(cp.base.getCurrencyCode());
         this.quoteCurrency = new CurrencyDTO(cp.counter.getCurrencyCode());
+        this.baseCurrencyPrecision = DEFAULT_CURRENCY_PRECISION;
+        this.qouteCurrencyPrecision = DEFAULT_CURRENCY_PRECISION;
+    }
+
+    /**
+     * Constructor from XChange instrument.
+     *
+     * @param instrument                instrument
+     * @param newBaseCurrencyPrecision  the base currency precision
+     * @param newQouteCurrencyPrecision the qoute currency precision
+     */
+    public CurrencyPairDTO(final Instrument instrument, final int newBaseCurrencyPrecision, final int newQouteCurrencyPrecision) {
+        final CurrencyPair cp = (CurrencyPair) instrument;
+        this.baseCurrency = new CurrencyDTO(cp.base.getCurrencyCode());
+        this.quoteCurrency = new CurrencyDTO(cp.counter.getCurrencyCode());
+        this.baseCurrencyPrecision = newBaseCurrencyPrecision;
+        this.qouteCurrencyPrecision = newQouteCurrencyPrecision;
     }
 
     @Override
