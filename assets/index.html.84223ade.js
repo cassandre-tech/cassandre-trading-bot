10322f4a79b0d4262d9e00d9ed182881df3cba40
straumat <stephane.traumat@gmail.com>
2022-03-23 17:44:41 +0000

    Website update

---
+++ assets/index.html.84223ade.js
@@ -0,0 +1,13 @@
+import{r as s,o as c,c as l,a as e,w as t,F as p,b as n,d as a}from"./app.adf81b97.js";import{_ as i}from"./plugin-vue_export-helper.21dcd24c.js";const u={},m=n("h3",{id:"as-easy-as-1-2-3",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#as-easy-as-1-2-3","aria-hidden":"true"},"#"),a(" As Easy as 1, 2, 3")],-1),b=n("div",{class:"language-bash ext-sh line-numbers-mode"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token comment"},"# Create a simple project with our archetype"),a(`
+mvn archetype:generate `),n("span",{class:"token punctuation"},"\\"),a(`
+-DarchetypeGroupId`),n("span",{class:"token operator"},"="),a("tech.cassandre.trading.bot "),n("span",{class:"token punctuation"},"\\"),a(`
+-DarchetypeArtifactId`),n("span",{class:"token operator"},"="),a("cassandre-trading-bot-spring-boot-starter-basic-archetype "),n("span",{class:"token punctuation"},"\\"),a(`
+-DarchetypeVersion`),n("span",{class:"token operator"},"="),n("span",{class:"token number"},"5.0"),a(".8 "),n("span",{class:"token punctuation"},"\\"),a(`
+-DgroupId`),n("span",{class:"token operator"},"="),a("com.example "),n("span",{class:"token punctuation"},"\\"),a(`
+-DartifactId`),n("span",{class:"token operator"},"="),a("my-bot "),n("span",{class:"token punctuation"},"\\"),a(`
+-Dversion`),n("span",{class:"token operator"},"="),n("span",{class:"token number"},"1.0"),a("-SNAPSHOT "),n("span",{class:"token punctuation"},"\\"),a(`
+-Dpackage`),n("span",{class:"token operator"},"="),a(`com.example
+
+`),n("span",{class:"token comment"},"# Runs unit tests"),a(`
+mvn -f my-bot/pom.xml `),n("span",{class:"token builtin class-name"},"test"),a(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("span",{class:"line-number"},"1"),n("br"),n("span",{class:"line-number"},"2"),n("br"),n("span",{class:"line-number"},"3"),n("br"),n("span",{class:"line-number"},"4"),n("br"),n("span",{class:"line-number"},"5"),n("br"),n("span",{class:"line-number"},"6"),n("br"),n("span",{class:"line-number"},"7"),n("br"),n("span",{class:"line-number"},"8"),n("br"),n("span",{class:"line-number"},"9"),n("br"),n("span",{class:"line-number"},"10"),n("br"),n("span",{class:"line-number"},"11"),n("br"),n("span",{class:"line-number"},"12"),n("br")])],-1);function d(h,k){const o=s("CodeGroupItem"),r=s("CodeGroup");return c(),l(p,null,[m,e(r,null,{default:t(()=>[e(o,{title:"Command line",active:""},{default:t(()=>[b]),_:1})]),_:1})],64)}var g=i(u,[["render",d]]);export{g as default};
