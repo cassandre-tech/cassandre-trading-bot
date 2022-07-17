f45d2edb1328bec11821832f9756c0daa7324a70
straumat <stephane.traumat@gmail.com>
2022-07-17 17:36:46 +0000

    Website deployment

---
+++ assets/index.html.269ae5ec.js
@@ -0,0 +1,11 @@
+import{_ as r,o as l,c as i,a as n,w as s,b as e,d as a,r as t}from"./app.d5beeed6.js";const p={},d=e("h3",{id:"as-easy-as-1-2-3",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#as-easy-as-1-2-3","aria-hidden":"true"},"#"),a(" As Easy as 1, 2, 3")],-1),u=e("div",{class:"language-bash ext-sh line-numbers-mode"},[e("pre",{class:"language-bash"},[e("code",null,[a("mvn archetype:generate "),e("span",{class:"token punctuation"},"\\"),a(`
+-DarchetypeGroupId`),e("span",{class:"token operator"},"="),a("tech.cassandre.trading.bot "),e("span",{class:"token punctuation"},"\\"),a(`
+-DarchetypeArtifactId`),e("span",{class:"token operator"},"="),a("cassandre-trading-bot-spring-boot-starter-basic-archetype "),e("span",{class:"token punctuation"},"\\"),a(`
+-DarchetypeVersion`),e("span",{class:"token operator"},"="),e("span",{class:"token number"},"6.0"),a(".0 "),e("span",{class:"token punctuation"},"\\"),a(`
+-DgroupId`),e("span",{class:"token operator"},"="),a("com.example "),e("span",{class:"token punctuation"},"\\"),a(`
+-DartifactId`),e("span",{class:"token operator"},"="),a("my-bot "),e("span",{class:"token punctuation"},"\\"),a(`
+-Dversion`),e("span",{class:"token operator"},"="),e("span",{class:"token number"},"1.0"),a("-SNAPSHOT "),e("span",{class:"token punctuation"},"\\"),a(`
+-Dpackage`),e("span",{class:"token operator"},"="),a(`com.example
+
+mvn -f my-bot/pom.xml `),e("span",{class:"token builtin class-name"},"test"),a(`
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1);function m(b,_){const o=t("CodeGroupItem"),c=t("CodeGroup");return l(),i("div",null,[d,n(c,null,{default:s(()=>[n(o,{title:"Command line",active:""},{default:s(()=>[u]),_:1})]),_:1})])}var k=r(p,[["render",m],["__file","index.html.vue"]]);export{k as default};
